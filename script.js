8c9dabbd69c4373729f632bd863c2ee04308c799
tijnvlith <66628263+tijnvlith@users.noreply.github.com>
2020-07-22 13:25:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+/*
+const ikRockArrowFunctions = function () {
+    console.log("Joe, ik rock de arrow functions!");
+}
+*/
+
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+
+/*
+const fivePlusSeven = function () {
+    return 5 + 7
+  };
+  
+*/
+
+
+() => 'Hello, World!'
+
+let myFunction = (a, b) => a + b;
+
+let myFunction2 = a => a + 5;
+
